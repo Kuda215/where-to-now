28f8da576c6803dedc8c239ecb5f840743dd312f
Kuda215 <142401509+Kuda215@users.noreply.github.com>
2024-12-10 14:54:11 +0200

    init

--- src/firebase.js
+++ src/firebase.js
@@ -8,16 +8,18 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAg23nfLZ0yCj4zzJwRYyfKVQ-4gaNSgVo",
-  authDomain: "where-to-now-f9fa3.firebaseapp.com",
-  projectId: "where-to-now-f9fa3",
-  storageBucket: "where-to-now-f9fa3.firebasestorage.app",
-  messagingSenderId: "676586669636",
-  appId: "1:676586669636:web:fda26a472faea109881e11",
-  measurementId: "G-F76TJWX301"
+  apiKey: "AIzaSyCWVCtDZqNNQiv2X5rTVZqqxcfT8Nm80tk",
+  authDomain: "where-to-now-7d4b9.firebaseapp.com",
+  projectId: "where-to-now-7d4b9",
+  storageBucket: "where-to-now-7d4b9.firebasestorage.app",
+  messagingSenderId: "579291111250",
+  appId: "1:579291111250:web:09d0770ac7eb4de9b1648b",
+  measurementId: "G-K9Y2C51CMM"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 //const analytics = getAnalytics(app);
 export const auth = getAuth(app);
+
+
